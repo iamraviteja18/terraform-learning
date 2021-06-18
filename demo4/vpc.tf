2cb473135affa45a355485698d7a3fc56f348cae
Ravi Teja Rajavarapu <50618333+iamraviteja18@users.noreply.github.com>
2021-06-18 16:38:01 +0530

    Update vpc.tf

--- demo4/vpc.tf
+++ demo4/vpc.tf
@@ -12,11 +12,19 @@ provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
-resource "aws_vpc" "trailvcp" {
+resource "aws_vpc" "trailvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "ravi"
+  }
+}
+resource "aws_subnet" "trailsubnet" {
+  vpc_id     = aws_vpc.trailvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "ravisub"
   }
 }

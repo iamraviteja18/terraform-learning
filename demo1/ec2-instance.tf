da2cfa063f91ad33e47ba84308ef540ba30caeff
Ravi Teja Rajavarapu <50618333+iamraviteja18@users.noreply.github.com>
2021-06-18 14:35:41 +0530

    Update ec2-instance.tf

--- demo1/ec2-instance.tf
+++ demo1/ec2-instance.tf
@@ -1,23 +1,24 @@
-terraform{
+terraform {
   required_providers {
-    aws={
-      source= "hashicorp/aws"
-      version= ">= 3.27"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
-  }  
+  }
+
   required_version = ">= 0.14.9"
 }
 
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
-  
-  tags={
-    Name = "Ravi"
+
+  tags = {
+    Name = "Arjun"
   }
 }

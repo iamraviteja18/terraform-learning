c45c86fde9c6ae5af2e79ffbfa5b7d5427b3186d
Ravi Teja Rajavarapu <50618333+iamraviteja18@users.noreply.github.com>
2021-06-18 16:29:10 +0530

    Update vpc.tf

--- demo4/vpc.tf
+++ demo4/vpc.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
 resource "aws_vpc" "trailvcp" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"

3f620888b3449e298cad13f8d34a334df0ddd505
Ravi Teja Rajavarapu <50618333+iamraviteja18@users.noreply.github.com>
2021-06-19 10:42:39 +0530

    Update instance.tf

--- demo5/instance.tf
+++ demo5/instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example18"{
   subnet_id = aws_subnet.main-public-1.id
   
   # this is for security group
-  vpc_security_groups_id = [aws_security_group.allow-ssh.id]
+  vpc_security_groups_ids = [aws_security_group.allow-ssh.id]
   
   # this is for public SSH key
   key_name = aws_key_pair.mykeypair.key_name
